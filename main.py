5590182e52058b9d84677ed051cdc5e3335cf1e8
katya <katy.zaitsewa2207@ya.ru>
2020-05-10 14:32:27 +0300

    ready2

--- main.py
+++ main.py
@@ -258,7 +258,8 @@ def apteka():
 
 def main():
     db_session.global_init("db/patients.sqlite")
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 class RegisterForm(FlaskForm):
